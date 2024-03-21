6202158dbf55ca256e6bc7e8a65b4d336dfe85ee
Teamcellmania <teamcellmania@Teamcellmanias-MacBook-Air.local>
2024-03-21 18:10:53 -0500

    :sparkles: Add configs for website

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,21 @@
+import type { Site } from './types'
 
 export const SITE_TITLE = "TeamCellmania"
 export const SITE_DESCRIPTION= "A curated list of the tech I use"
 export const HOMEPAGE_URL = "https://teamcellmania.com"
+
+export const SITE: Site = {
+  website: "https://teamcellmania.com", // replace this with your deployed domain
+  author: "rcraftzy",
+  desc: "A curated list of the tech I use",
+  title: "TeamCellmania",
+  // ogImage: "astropaper-og.jpg",
+  // lightAndDarkMode: true,
+  postPerPage: 12,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+};
+
+export const LOCALE = {
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+} as const;
