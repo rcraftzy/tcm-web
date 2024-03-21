d3db23df5b78a6e0fe6df622cef7ff7a36b8cefe
Teamcellmania <teamcellmania@Teamcellmanias-MacBook-Air.local>
2024-03-21 18:03:52 -0500

    :label: Add types

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+
+export type Site = {
+  website: string;
+  author: string;
+  desc: string;
+  title: string;
+  // ogImage?: string;
+  // lightAndDarkMode: boolean;
+  postPerPage: number;
+  scheduledPostMargin: number;
+};
