682371fb4582b1b867c83582b72e5edb759804d2
Teamcellmania <teamcellmania@Teamcellmanias-MacBook-Air.local>
2024-03-22 12:09:54 -0500

    :wrench: Add config for astro db, and create table for save videos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,26 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import addClasses from 'rehype-add-classes'
-
+import addClasses from 'rehype-add-classes';
 import react from "@astrojs/react";
 import { remarkReadingTime } from './remark-reading-time.mjs';
 
+import db from "@astrojs/db";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), db()],
   markdown: {
     syntaxHighlight: 'prism',
     gfm: true,
     remarkPlugins: [remarkReadingTime],
-    rehypePlugins: [
-      [
-        addClasses,
-        {
-          h1: "text-4xl font-bold front-mplus",
-          h2: "text-2xl font-bold front-mplus",
-          h3: "text-xl font-bold front-mplus",
-          h4: "text-lg font-bold front-mplus",
-          h5: "font-bold front-mplus",
-          h5: "font-bold front-mplus",
-          p: "mb-6",
-        }
-      ]
-    ],
+    rehypePlugins: [[addClasses, {
+      h1: "text-4xl font-bold front-mplus",
+      h2: "text-2xl font-bold front-mplus",
+      h3: "text-xl font-bold front-mplus",
+      h4: "text-lg font-bold front-mplus",
+      h5: "font-bold front-mplus",
+      h5: "font-bold front-mplus",
+      p: "mb-6"
+    }]]
   }
 });
---
+++ db/seed.ts
@@ -0,0 +1,151 @@
+import { Videos, db } from 'astro:db';
+
+// https://astro.build/db/seed
+export default async function seed() {
+  await db.insert(Videos).values([
+    {
+      id: "WbcR9c7fI-4",
+      title: "SAMSUNG A12 cambio de GLASS",
+      description: "",
+      link: "https://youtu.be/WbcR9c7fI-4",
+      published: "2024-03-22",
+      viewTime: "10:0",
+      tags: [{name: "hola"}, {name:"reparación"}] 
+    },
+    {
+      id: "ZgVzy7VhDmg",
+      title: "Publicidad Mando",
+      description: "",
+      link: "https://youtu.be/ZgVzy7VhDmg",
+      published: "2024-03-22",
+      viewTime: "10:00",
+      tags: [{name: "ecudacion"}, {name:"reparación"}] 
+    },
+    {
+      id: "tdwLM0vPQAI",
+      title: "IPHONE 12 PRO MAX!!! CAMBIO DE LENTE DE CÁMARA..!!!",
+      description: "",
+      link: "https://youtu.be/tdwLM0vPQAI",
+      published: "2024-03-22",
+      viewTime: "10:00",
+      tags: [{name: "ecudacion"}, {name:"reparación"}] 
+    },
+    {
+      id: "qYTHmRiwrqI",
+      title: "iPhone 12 Pro Max..!! CAMBIO de TAPA",
+      description: "",
+      link: "https://youtu.be/qYTHmRiwrqI",
+      published: "2024-03-22",
+      viewTime: "10:00",
+      tags: [{name: "ecudacion"}, {name:"reparación"}] 
+    },
+    {
+      id: "HL8d82fDpyM",
+      title: "iPhone 11 Pro Max..!! CAMBIO de TAPA",
+      description: "",
+      link: "https://youtu.be/HL8d82fDpyM",
+      published: "2024-03-22",
+      viewTime: "10:00",
+      tags: [{name: "ecudacion"}, {name:"reparación"}] 
+    },
+    {
+      id: "YxPfx-W9ktc",
+      title: "ALARGA la VIDA a tus DIPOSITIVOS.. la PROTECCIÒN es PRIMERO..!!",
+      description: "",
+      link: "https://youtu.be/YxPfx-W9ktc",
+      published: "2024-03-22",
+      viewTime: "10:00",
+      tags: [{name: "ecudacion"}, {name:"reparación"}] 
+    },
+    {
+      id: "dgXIwP5iuEQ",
+      title: "SAMSUNG Galaxy J6 Plus..!! CAMBIO de GLASS",
+      description: "",
+      link: "https://youtu.be/dgXIwP5iuEQ",
+      published: "2024-03-22",
+      viewTime: "10:00",
+      tags: [{name: "ecudacion"}, {name:"reparación"}] 
+    },
+    {
+      id: "kt-8lKni8VQ",
+      title: "NO DESCUIDES tu TELÉFONO de estos CARNAVALES..!!!",
+      description: "",
+      link: "https://youtu.be/kt-8lKni8VQ",
+      published: "2024-03-22",
+      viewTime: "10:00",
+      tags: [{name: "ecudacion"}, {name:"reparación"}] 
+    },
+    {
+      id: "8zCC7D4Mm9M",
+      title: "iPhone 8 Plus MOJADO...!! ELIMINACIÓN de HUMEDAD y cambio BLACKLIGTH marcado por LÍQUIDO.",
+      description: "",
+      link: "https://youtu.be/8zCC7D4Mm9M",
+      published: "2024-03-22",
+      viewTime: "10:00",
+      tags: [{name: "ecudacion"}, {name:"reparación"}] 
+    },
+    {
+      id: "27MnzhfPNk4",
+      title: "HUAWEI Y5 2019..!! DIGITALIZADOR de TOUCH no responde a solicitud de SENSIBILIDAD.",
+      description: "",
+      link: "https://youtu.be/27MnzhfPNk4",
+      published: "2024-03-22",
+      viewTime: "10:00",
+      tags: [{name: "ecudacion"}, {name:"reparación"}] 
+    },
+    {
+      id: "3AUvRBm5syU",
+      title: "IPHONE 6..!! como PASAR de tener 16GB a tener 128GB...!!",
+      description: "",
+      link: "https://youtu.be/3AUvRBm5syU",
+      published: "2024-03-22",
+      viewTime: "10:00",
+      tags: [{name: "ecudacion"}, {name:"reparación"}] 
+    },
+    {
+      id: "jGrTIRpoqO0",
+      title: "SAMSUNG A10 MOJADO!!! CORRE a TeamCellmania INMEDIATAMENTE..!!!!",
+      description: "",
+      link: "https://youtu.be/jGrTIRpoqO0",
+      published: "2024-03-22",
+      viewTime: "10:00",
+      tags: [{name: "ecudacion"}, {name:"reparación"}] 
+    },
+    {
+      id: "4XV68KfelDA",
+      title: "iPhone 8 Plus... cambio de BATERIA & GLASS!!",
+      description: "",
+      link: "https://youtu.be/4XV68KfelDA",
+      published: "2024-03-22",
+      viewTime: "10:00",
+      tags: [{name: "ecudacion"}, {name:"reparación"}] 
+    },
+    {
+      id: "9Vd0kAX0IFM",
+      title: "Llego la Navidad a TeamCellmania!!!",
+      description: "",
+      link: "https://youtu.be/9Vd0kAX0IFM",
+      published: "2024-03-22",
+      viewTime: "10:00",
+      tags: [{name: "ecudacion"}, {name:"reparación"}] 
+    },
+    {
+      id: "8LH2URpp5Uw",
+      title: "SAMSUNG A32 no FUNCIONA el TACTIL!!! EXPLICACIÓN y SOLUCIÓN!!!",
+      description: "",
+      link: "https://youtu.be/8LH2URpp5Uw",
+      published: "2024-03-22",
+      viewTime: "10:00",
+      tags: [{name: "ecudacion"}, {name:"reparación"}] 
+    },
+    {
+      id: "H7k1CIp9WFs",
+      title: "iPhone X con PROBLEMAS en FLASH y CAMARA!!!",
+      description: "",
+      link: "https://youtu.be/H7k1CIp9WFs",
+      published: "2024-03-22",
+      viewTime: "10:00",
+      tags: [{name: "ecudacion"}, {name:"reparación"}] 
+    },
+  ])
+}
