365b3dbf97f92cb94b7658d8dee56c83c270a938
Teamcellmania <teamcellmania@Teamcellmanias-MacBook-Air.local>
2024-03-29 13:33:31 -0500

    🔧 Add config to images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, squooshImageService } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import addClasses from 'rehype-add-classes';
 import react from "@astrojs/react";
@@ -9,17 +9,20 @@ import db from "@astrojs/db";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), db()],
+  image: {
+    service: squooshImageService()
+  },
   markdown: {
     syntaxHighlight: 'prism',
     gfm: true,
     remarkPlugins: [remarkReadingTime],
     rehypePlugins: [[addClasses, {
-      h1: "text-4xl font-bold front-mplus",
-      h2: "text-2xl font-bold front-mplus",
-      h3: "text-xl font-bold front-mplus",
-      h4: "text-lg font-bold front-mplus",
-      h5: "font-bold front-mplus",
-      h5: "font-bold front-mplus",
+      h1: "font-mplus mb-16",
+      h2: "text-4xl font-semibold font-mplus mb-2",
+      h3: "text-2xl font-bold font-mplus mb-2",
+      h4: "text-lg font-bold font-mplus",
+      h5: "font-bold font-mplus",
+      h6: "font-bold font-mplus",
       p: "mb-6"
     }]]
   }
