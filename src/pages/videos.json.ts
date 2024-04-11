c6717d80aba4fcb0f49f111c79f341109725cffe
Teamcellmania <teamcellmania@Teamcellmanias-MacBook-Air.local>
2024-04-11 09:28:49 -0500

    ✨ Add api endpoint for videos and implementate in pages to videos

---
+++ src/pages/videos.json.ts
@@ -0,0 +1,24 @@
+import { Videos, db } from "astro:db";
+import type { APIRoute } from 'astro'
+
+export const GET: APIRoute = async () => {
+  const videos = await db.select().from(Videos);
+
+  if (!videos) {
+    return new Response(null, {
+      status: 404,
+      statusText: 'Not found',
+    })
+  }
+
+  console.log(videos)
+
+  return new Response(
+    JSON.stringify(videos), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }
+  )
+}
