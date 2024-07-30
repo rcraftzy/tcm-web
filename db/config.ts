303ffe55b6e18cf70a230f6d4cfe34ff3809984e
Teamcellmania <teamcellmania@Teamcellmanias-MacBook-Air.local>
2024-07-30 11:25:08 -0500

    Add tables for locations sucursales

--- db/config.ts
+++ db/config.ts
@@ -1,18 +1,26 @@
-import { column, defineDb, defineTable } from 'astro:db';
+import { column, defineDb, defineTable } from "astro:db";
 
 // https://astro.build/db/config
 const Videos = defineTable({
-  columns: {
-    id: column.text({primaryKey: true}),
-    title: column.text(),
-    description: column.text(),
-    link: column.text(),
-    published: column.text(),
-    viewTime: column.text(),
-    tags: column.json()
-  }
-})
+	columns: {
+		id: column.text({ primaryKey: true }),
+		title: column.text(),
+		description: column.text(),
+		link: column.text(),
+		published: column.text(),
+		viewTime: column.text(),
+		tags: column.json(),
+	},
+});
+
+const Locations = defineTable({
+	columns: {
+		id: column.text({ primaryKey: true }),
+		coords: column.json(),
+		name: column.text(),
+	},
+});
 
 export default defineDb({
-  tables: { Videos }
+	tables: { Videos, Locations },
 });
